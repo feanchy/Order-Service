bb682d27fd65b9255e2b4ec079df38c2eb9db385
feanchy <feanchy@MacBook-Pro-feanchy.local>
2026-09-24 01:32:46 +0300

    feat: implement order repository

--- internal/repository/postgres/order.go
+++ internal/repository/postgres/order.go
@@ -18,11 +18,17 @@ func NewOrderRepository(db *pgxpool.Pool) *OrderRepository {
 }
 
 func (r *OrderRepository) GetByID(ctx context.Context, id int) (*model.Order, error) {
-	query := `SELECT id, status FROM orders WHERE id = $1`
+	const query = `
+	SELECT id, status 
+	FROM orders
+	WHERE id = $1`
 
 	var order model.Order
 
-	err := r.db.QueryRow(ctx, query, id).Scan(&order.ID, &order.Status)
+	err := r.db.QueryRow(ctx, query, id).Scan(
+		&order.ID,
+		&order.Status,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -32,14 +38,49 @@ func (r *OrderRepository) GetByID(ctx context.Context, id int) (*model.Order, er
 }
 
 // f
-func (r *OrderRepository) Create(ctx context.Context, id int) (int, error) {
+func (r *OrderRepository) Create(ctx context.Context, order model.Order) (int, error) {
+	const query = `
+        INSERT INTO orders (status)
+        VALUES ($1)
+        RETURNING id
+    `
 
+	var id int
+
+	err := r.db.QueryRow(ctx, query, order.Status).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
 
-func (r *OrderRepository) UpdateStatus(ctx context.Context, id int) (int, error) {
+func (r *OrderRepository) UpdateStatus(ctx context.Context, order model.Order) error {
+	const query = `
+	UPDATE orders
+	SET status = $1
+	WHERE id = $2
+	`
 
+	_, err := r.db.Exec(ctx, query, order.Status, order.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (r *OrderRepository) Delete(ctx context.Context, id int) (int, error) {
+func (r *OrderRepository) Delete(ctx context.Context, id int) error {
+	const query = `
+	DELETE FROM orders
+	WHERE id = $1
+	`
+
+	_, err := r.db.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
